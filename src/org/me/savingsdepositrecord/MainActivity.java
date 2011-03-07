75372713c0895da15bda64d01a62632dcddb3a29
Antony Evans <antonyevans@gmail.com>
2011-03-07 12:49:37 -0800

    First Commit

---
+++ src/org/me/savingsdepositrecord/MainActivity.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ * This file was created by Antony Evans for Samual
+ * If you read this Samual please email me codeword 'github works!!'
+ */
+
+package org.me.savingsdepositrecord;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ *
+ * @author Antony
+ */
+public class MainActivity extends Activity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        // ToDo add your GUI initialization code here        
+    }
+
+}
